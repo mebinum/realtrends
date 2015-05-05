27808520825f934e50e7fc85b7d4e8148c33683c
Mike Ebinum <mebinum@gmail.com>
2015-05-05 13:56:54 +1000

    added docker deploy script and updated readme with instructions

--- README.md
+++ README.md
@@ -10,8 +10,16 @@ To start the application pass the variable via the command line or set it in the
 
 i.e
             ES_SERVER_URL=10.0.0.0 DEBUG=app npm start
+			
+indexes for elastic search
+
+	building_data
 
 ## With Docker
+
+if you are running the elasticsearch docker container use the command to start the service
+
+		 docker run -d -v /path/to/elasticsearch/data/r:/usr/share/elasticsearch/data -p 9200:9200 --name realtrends_es rt-elasticsearch
 
 if running on a local docker container use the `-e` tag to set the environment variables
 
@@ -19,7 +27,10 @@ e.g.
 
         docker run --rm -p 8080:3000 -v /path/to/projects/realtrendz/app:/app -e "ES_SERVER_URL=IP_OF_ES_SERVER_INSTANCE:9200" --name realtrends_web realtrendz_web
 
-	
-indexes
+#Deploying on a production server
 
-	building_data
+Log on to the production server for realtrends and pull down the latest image for `realtrends`
+
+		docker pull docker-registry/realtrends_web
+	
+pull down the latest code changes from the github repo and run the deploy bash script at `devops/docker_deploy.sh`
---
+++ devops/docker_deploy.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set +e
+
+#set fail on error
+#set -e -x
+
+#Adapted from https://blog.harbur.io/continuous-delivery-ghost/
+# to change the domains to be bound use `docker_deploy.sh 'somedomain.com' '/path/to/elasticsearch/data'` 
+APP_NAME=realtrends_web
+APP_BLUE=${APP_NAME}"_blue"
+APP_GREEN=${APP_NAME}"_green"
+APP_IMAGE_NAME=docker-registry.seeddigital.co/realtrends_web
+NGINX_PROXY=realtrends_proxy
+ES_IMAGE_NAME=docker-registry.seeddigital.co/realtrends_elasticsearch
+ES_CONAINTER_NAME=realtrends_es
+DEFAULT_DOMAINS=*.realtrends.co
+HOST_DOMAIN=$([ -z "$1" ] && echo $DEFAULT_DOMAINS || echo $DEFAULT_DOMAINS","$1)
+ES_DATA_PATH_DEFAULT=$(pwd)"/esdatas"
+ES_DATA_PATH=${2:-$ES_DATA_PATH_DEFAULT}
+
+es_port () {
+  PORT=$(docker inspect -f '{{(index ( index .NetworkSettings.Ports "9200/tcp") 0).HostPort}}' ${ES_CONAINTER_NAME})
+  echo $PORT
+}
+
+is_container_running () {
+  [[ $(docker inspect -f "{{.State.Running}}" $1) = "true" ]] && up=1 || up=0
+  echo $up
+}
+
+run_container() {
+  NAME=$1
+  PORT=$(es_port)
+  docker run -d -e VIRTUAL_HOST=$HOST_DOMAIN -e VIRTUAL_PORT=3000  -e "ES_SERVER_URL=realtrends.co:${PORT}" --name $NAME -P -v /data/logs:/data/logs -d $APP_IMAGE_NAME
+}
+
+
+
+#check if nginx proxy container is not running if it isn't start it
+
+if [ $(is_container_running ${NGINX_PROXY}) -eq 0 ]; then
+    echo "Staring nginx proxy ${NGINX_PROXY}"
+    docker rm ${NGINX_PROXY}
+    docker run --name ${NGINX_PROXY} -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock -t jwilder/nginx-proxy
+fi
+
+# start the elastic search container if it's not running
+if [ $(is_container_running ${ES_CONAINTER_NAME}) -eq 0 ]; then
+    echo "Staring elastic search container ${ES_CONAINTER_NAME} with image ${ES_IMAGE_NAME}"
+    docker rm ${ES_CONAINTER_NAME}
+    docker run -d -v ${ES_DATA_PATH}:/usr/share/elasticsearch/data -P --name ${ES_CONAINTER_NAME} ${ES_IMAGE_NAME}
+fi
+
+# Check if green instance is running
+if [ $(is_container_running ${APP_GREEN}) -eq 0 ]; then  
+  echo "Currently active node is ${APP_BLUE}, deploying ${APP_GREEN}"
+  docker rm ${APP_GREEN}
+  run_container ${APP_GREEN}
+  sleep 20
+  docker stop ${APP_BLUE}
+else  
+  echo "Currently active node is ${APP_GREEN}, deploying ${APP_BLUE}"
+  docker rm ${APP_BLUE}
+  run_container ${APP_BLUE}
+  sleep 20
+  docker stop ${APP_GREEN}
+fi  
